92a262c60edc8d775772225fbf9ca485e783e8c7
Jorge Soares <jorgejunioalvessoares@hotmail.com>
2021-06-27 02:26:35 -0300

    Sync App Files

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:azza_telecom_clientes/views/Home.dart';
+
+final ThemeData temaPadrao = ThemeData(
+  //primaryColor: Color.fromRGBO(39, 52, 139, 1),
+  accentColor: Color(0xffeb5847),
+);
+
+void main() => runApp(MaterialApp(
+    title: "Azza Telecom",
+    home: Home(),
+    theme: temaPadrao,
+    debugShowCheckedModeBanner: false,
+));
